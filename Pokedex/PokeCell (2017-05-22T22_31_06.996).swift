e5f9f605800b9dbcd756eeaf764a1dfd66f939fb
pegasusCK <cky1026@gmail.com>
2017-05-22 15:37:39 -0700

    Lesson 89 Custom collection view cell

---
+++ Pokedex/PokeCell (2017-05-22T22_31_06.996).swift
@@ -0,0 +1,17 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Chihkao Yu on 5/22/17.
+//  Copyright © 2017 Chihkao Yu. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    
+}
